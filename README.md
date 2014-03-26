219d148d23fa63145090266d3de6b01de9cb49c0
neiluJ <julien@nitronet.org>
2014-03-26 17:58:43 +0100

    started development

---
+++ README.md
@@ -0,0 +1,33 @@
+# fwk-assetic
+
+Adds [Assetic](https://github.com/kriswallsmith/assetic) to [Fwk\Core](https://github.com/fwk/Core) Applications.
+
+## Installation
+
+### 1: Install the sources
+
+Via [Composer](http://getcomposer.org):
+
+```
+{
+    "require": {
+        "nitronet/fwk-assetic": "dev-master",
+    }
+}
+```
+
+If you don't use Composer, you can still [download](https://github.com/nitronet/fwk-assetic/zipball/master) this repository and add it
+to your ```include_path``` [PSR-0 compatible](https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-0.md)
+
+### 2: Defines Assetic as a Service
+
+Define a ```Twig_Environment``` instance in your [Di](https://github.com/fwk/Di) Container, and configure it the way you want:
+
+``` xml
+
+```
+
+## Contributions / Community
+
+- Issues on Github: https://github.com/nitronet/fwk-assetic/issues
+- Follow *Fwk* on Twitter: [@phpfwk](https://twitter.com/phpfwk)
---
+++ src/FwkAssetic/Controllers/AssetAction.php
@@ -0,0 +1,140 @@
+<?php
+namespace FwkAssetic\Controllers;
+
+use Fwk\Core\ServicesAware;
+use Fwk\Core\ContextAware;
+use Fwk\Di\Container;
+use Symfony\Component\HttpFoundation\Response;
+use Fwk\Core\Context;
+use Assetic\Asset\AssetCache;
+use Assetic\Cache\FilesystemCache;
+
+class AssetAction implements ServicesAware, ContextAware
+{
+    public $asset;
+    
+    protected $services;
+    protected $context;
+    
+    public function show()
+    {
+        $assetic    = $this->getServices()->get('assetic.AssetFactory');
+        $response   = new Response();
+        $response->setExpires(new \DateTime());
+        if (!$assetic instanceof \Assetic\Factory\AssetFactory) {
+            $response->setStatusCode(500);
+            return $response;
+        }
+        
+        if (empty($this->asset) || strpos($this->asset, '/') === false) {
+            $response->setStatusCode(400);
+            return $response;
+        }
+        
+        list(,$formulaName,) = explode('/', $this->asset);
+        
+        // asset not found
+        if (empty($formulaName) || !$assetic->getAssetManager()->has($formulaName)) {
+            $response->setStatusCode(404);
+            return $response;
+        }
+        
+        $formula     = $assetic->getAssetManager()->getFormula($formulaName);
+        if ($formula[0] instanceof \Assetic\Asset\AssetInterface) {
+            $asset   = $formula[0];
+        } else {
+            $asset   = $assetic->createAsset($formula[0], $formula[1], $formula[2]);
+        } 
+        
+        if (null !== $lastModified = $asset->getLastModified()) {
+            $date = new \DateTime();
+            $date->setTimestamp($lastModified);
+            $response->setLastModified($date);
+        }
+
+        $formula['last_modified'] = $lastModified;
+        $response->setETag(md5(serialize($formula)));
+        
+        $this->defineContentType($response);
+        
+        if ($response->isNotModified($this->getContext()->getRequest())) {
+            return $response;
+        }
+        
+        $response->setContent($this->cacheAsset($asset)->dump());
+        
+        return $response;
+    }
+    
+    /**
+     * 
+     * @param AssetInterface $asset
+     * 
+     * @return AssetInterface 
+     */
+    protected function cacheAsset(AssetInterface $asset)
+    {
+        $services   = $this->getServices();
+        $useCache   = false; /* TODO */
+        
+        if (!$useCache) {
+            return $asset;
+        }
+        
+        $dir        = $services->getProperty('assetic.cache.directory');
+        return new AssetCache(
+            $asset, 
+            new FilesystemCache($dir)
+        );
+    }
+    
+    /**
+     * Defines the content type header on the Response according to the
+     * required asset name (@see $this->asset)
+     * 
+     * @param Response $response To-be-returned HTTP Response
+     * 
+     * @return void
+     */
+    protected function defineContentType(Response $response)
+    {
+        if(strpos($this->asset, '.js', strlen($this->asset)-3)) {
+            $response->headers->set('Content-Type', 'text/javascript');
+        } elseif(strpos($this->asset, '.css', strlen($this->asset)-4)) {
+            $response->headers->set('Content-Type', 'text/css');
+        } elseif(strpos($this->asset, '.png', strlen($this->asset)-4)) {
+            $response->headers->set('Content-Type', 'image/png');
+        } elseif(strpos($this->asset, '.jpg', strlen($this->asset)-4)
+                || strpos($this->asset, '.jpeg', strlen($this->asset)-5)) {
+            $response->headers->set('Content-Type', 'image/jpeg');
+        } elseif(strpos($this->asset, '.gif', strlen($this->asset)-4)) {
+            $response->headers->set('Content-Type', 'image/gif');
+        } elseif(strpos($this->asset, '.ttf', strlen($this->asset)-4)) {
+            $response->headers->set('Content-Type', 'application/font-ttf');
+        } elseif(strpos($this->asset, '.woff', strlen($this->asset)-5)) {
+            $response->headers->set('Content-Type', 'application/font-woff');
+        } elseif(strpos($this->asset, '.svg', strlen($this->asset)-4)) {
+            $response->headers->set('Content-Type', 'application/svg+xml');
+        }
+    }
+    
+    public function getServices()
+    {
+        return $this->services;
+    }
+
+    public function setServices(Container $services)
+    {
+        $this->services = $services;
+    }
+    
+    public function getContext()
+    {
+        return $this->context;
+    }
+
+    public function setContext(Context $context)
+    {
+        $this->context = $context;
+    }
+}
